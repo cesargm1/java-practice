64020b45ebdc00a5d5b3349473f52d77e94d5265
cesar <cg.gtav@gmail.com>
2024-09-19 12:14:29 +0200

    compilador

---
+++ pratices_school/compilador/src/compliadorA.java
@@ -0,0 +1,18 @@
+public class compliadorA {
+    public static void main(String[] args) throws Exception {
+
+        int x = 1;
+
+        while (x < 10) {
+            if (x < 3) {
+                System.out.println("big x");
+                x++;
+            }
+
+        }
+    }
+}
+
+// anterior x > 3 actual x < 3
+
+// añadir incremento x++; si no bucle infinito
