c733cc1f462d5fa2d269e9c02d010d4731a55076
cesar <cg.gtav@gmail.com>
2024-10-24 12:34:49 +0200

    cuenta corriente

--- pratices_school/cuenta corriente/src/App.java
+++ pratices_school/cuenta corriente/src/App.java
@@ -1,5 +1,43 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Cajero cajero = new Cajero();
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
+            try {
+                System.out.println("Seleccione una operación:");
+                System.out.println("1. Ingresar dinero");
+                System.out.println("2. Consultar saldo");
+                System.out.println("3. Salir");
+
+                int opcion = scanner.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        cajero.ingresar();
+                        break;
+                    case 2:
+                        cajero.obtenerSaldo();
+                        break;
+                    case 3:
+                        exit = true;
+                        cajero.cerrarScanner();
+                        System.out.println("Gracias por usar el cajero.");
+                        break;
+                    default:
+                        System.out.println("Opción no válida, intente de nuevo.");
+                }
+            } catch (InputMismatchException e) {
+
+                System.out.println("Error: Debes ingresar un número entero.");
+                scanner.next();
+            }
+        }
+
+        scanner.close();
     }
 }
